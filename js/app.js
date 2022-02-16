aa701288dbaaeeeef6d01dcbc66ba3176cc729d0
jahid4striver <jahdihassan701@gmail.com>
2022-02-17 02:10:52 +0600

    Almost Done UI Last Fininshing Need

--- js/app.js
+++ js/app.js
@@ -1,49 +1,67 @@
-const calculateBtn = document.getElementById('calculate-btn');
+// Functions
 
+function getId(idName) {
+    return document.getElementById(idName);
+}
+function toNum(value) {
+    return parseFloat(value);
+}
+
+// Calculate Button Events
+const calculateBtn = getId('calculate-btn');
 calculateBtn.addEventListener('click', function () {
+    const income = getId('income');
+    const incomeValue = toNum(income.value);
+    const food = getId('food');
+    const foodValue = toNum(food.value);
+    const rent = getId('rent');
+    const rentValue = toNum(rent.value);
+    const cloth = getId('cloth');
+    const clothValue = toNum(cloth.value);
 
-    const income = document.getElementById('income');
-    const incomeValue = parseFloat(income.value);
-    const food = document.getElementById('food');
-    const foodValue = parseFloat(food.value);
-    const rent = document.getElementById('rent');
-    const rentValue = parseFloat(rent.value);
-    const cloth = document.getElementById('cloth');
-    const clothValue = parseFloat(cloth.value);
+    // Error Handeling
+    const negError = getId('negetive');
+    const stringError = getId('blank-error');
+    const extraError = getId('extra-expense')
+    if (incomeValue < 0 || foodValue < 0 || rentValue < 0 || clothValue < 0) {
+        negError.style.display = 'block';
+    } else if (isNaN(incomeValue) || isNaN(foodValue) || isNaN(rentValue) || isNaN(clothValue)) {
+        stringError.style.display = 'block';
+    } else {
+        const totalExpenses = getId('total-expenses').innerText = foodValue + rentValue + clothValue;
+        const balanceAfterExp = getId('balance-after-expense');
+        if (totalExpenses > incomeValue) {
+            extraError.style.display = 'block';
+        } else {
+            balanceAfterExp.innerText = toNum(incomeValue - totalExpenses);
+        }
 
-    // console.log(incomeValue, foodValue, rentValue, clothValue)
-    // // const incomeValue = income.value;
-    // console.log(income);
-
-    const totalExpenses = document.getElementById('total-expenses').innerText = foodValue + rentValue + clothValue;
-    console.log(totalExpenses);
-    const balanceAfterExp = document.getElementById('balance-after-expense');
-    balanceAfterExp.innerText = parseFloat(incomeValue - totalExpenses);
+    }
 });
 
 
-const saveBtn = document.getElementById('save-btn');
+// Save Money Button Events
 
+const saveBtn = getId('save-btn');
 saveBtn.addEventListener('click', function () {
-    const income = document.getElementById('income');
-    const incomeValue = parseFloat(income.value);
-    const save = document.getElementById('save');
-    const saveValue = parseFloat(save.value);
-    const saving = document.getElementById('saving-amount');
-    const savingValue = parseFloat(saving.value);
-    const remaining = document.getElementById('remain-amount');
-    const remainingValue = parseFloat(remaining.value);
+    const income = getId('income');
+    const incomeValue = toNum(income.value);
+    const save = getId('save');
+    const saveValue = toNum(save.value);
+    const saving = getId('saving-amount');
+    const remaining = getId('remain-amount');
+
+    const balanceAfterExp = getId('balance-after-expense');
+    const balanceAfterExpValue = toNum(balanceAfterExp.innerText);
+    const saveMoney = toNum(incomeValue / 100 * saveValue);
+    // Error Handeling
+    if (saveMoney > balanceAfterExpValue) {
+        getId('saving-error').style.display = 'block'
+    } else {
+        saving.innerText = saveMoney;
+        remaining.innerText = balanceAfterExpValue - saveMoney;
+    }
+});
 
 
-    const balanceAfterExp = document.getElementById('balance-after-expense');
-    const balanceAfterExpValue = parseFloat(balanceAfterExp.innerText);
-    const saveMoney = parseFloat(incomeValue / 100 * saveValue);
-    saving.innerText = saveMoney;
-    remaining.innerText = balanceAfterExpValue - saveMoney;
 
-})
-
-
-// function idGetter(idName) {
-//     document.getElementById(idName);
-// }
